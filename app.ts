041b50535cb25acdc227f483b9ad9de8544c24f2
Punam Mohite <121328200+Punuuu03@users.noreply.github.com>
2024-11-08 12:31:06 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://adhikari-client-final.vercel.app","http://localhost:5173"],
+    origin: ["*"],
     methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
